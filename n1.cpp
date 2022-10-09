60191b1c0f1e3937b5386fdb520ab9411f2ee917
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2022-10-09 06:51:51 -0700

    Update n1.cpp

--- n1.cpp
+++ n1.cpp
@@ -1,4 +1,8 @@
+/**
 
+      Given an integer array nums sorted in non-decreasing
+      order, return an array of the squares of each number 
+      sorted in non-decreasing order.**/
 #include <iostream>
 #include<algorithm>
 
